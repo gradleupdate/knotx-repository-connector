cda2e6d3422bd0b71f9e2919c69deb06a1822f99
dragarth-7 <dragarth@o2.pl>
2020-02-11 14:11:50 +0100

    Make filtering allowed headers case-insensitive, wildcards

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@ All notable changes to `knotx-repository-connector` will be documented in this f
 
 ## Unreleased
 List of changes that are finished but not yet released in any final version.
+- [PR-10](https://github.com/Knotx/knotx-repository-connector/pull/10) - Make filtering allowed headers case-insensitive
 
 ## 2.1.0
 
--- http/src/main/java/io/knotx/repository/http/HttpRepositoryConnector.java
+++ http/src/main/java/io/knotx/repository/http/HttpRepositoryConnector.java
@@ -215,7 +215,7 @@ class HttpRepositoryConnector {
 
   private MultiMap filteredHeaders(MultiMap headers) {
     return headers.names().stream()
-        .filter(AllowedHeadersFilter.create(configuration.getAllowedRequestHeadersPatterns()))
+        .filter(AllowedHeadersFilter.WildcardCaseInsensitive.create(configuration.getAllowedRequestHeaders()))
         .collect(MultiMapCollector.toMultiMap(o -> o, headers::getAll));
   }
 
